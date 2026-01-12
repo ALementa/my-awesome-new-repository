1218ee94eadfe607fd958d299b3cd8716d1804d2
ALementa <lementalex@gmail.com>
2026-01-12 01:25:31 +0000

    Add button to top without JS

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+// function slowScrollToTop() {
+//   const duration = 1200;
+//   const start = window.scrollY;
+//   const startTime = performance.now();
+//   function scroll(curentTime) {
+//     const elapsed = curentTime - startTime;
+//     const progress = Math.min(elapsed / duration, 1);
+//     window.scrollTo(0, start * (1 - progress));
+//     if (progress < 1) {
+//       requestAnimationFrame(scroll);
+//     }
+//   }
+//   requestAnimationFrame(scroll);
+// }
+// const btn = document.getElementById("toTop"),
+//   onclick = slowScrollToTop;
+
 const btn = document.getElementById("toTop");
 
+window.addEventListener("scroll", () => {
+  btn.style.display = window.scrollY > 300 ? "block" : "none";
+});
 btn.addEventListener("click", () => {
-  document.window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-document.addEventListener(
-  "click",
-  () => {
-    const audio = document.getElementById("bg");
-    audio.play();
-  },
-  { once: true }
-);
+// const audioBG = document.getElementById("bg");
+
+// audioBG.addEventListener(
+//   "click",
+//   () => {
+//     const audio = document.getElementById("bg");
+//     audio.play();
+//   },
+//   { once: true }
+// );
